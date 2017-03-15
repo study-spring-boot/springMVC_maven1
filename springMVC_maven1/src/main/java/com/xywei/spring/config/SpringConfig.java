45663f1bb23403dba10653bbec515a16c400ed24
proxyei <insoul_net@qq.com>
2017-03-15 13:55:34 +0800

    2

---
+++ springMVC_maven1/src/main/java/com/xywei/spring/config/SpringConfig.java
@@ -0,0 +1,10 @@
+package com.xywei.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.xywei.*")
+public class SpringConfig {
+
+}
--- springMVC_maven1/src/main/resources/views/index.jsp
+++ springMVC_maven1/src/main/resources/views/index.jsp
@@ -6,6 +6,6 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
 </head>
-<body>welcome to springboot!
+<body>${message1},welcometo springboot!
 </body>
 </html>
